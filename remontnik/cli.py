d35f17fa164c4877a853a342c32f958de6cbae6d
Eugene Chekalov <evgchekalov@gmail.com>
2018-02-08 18:52:04 +0300

    parametrize cli command

--- remontnik/cli.py
+++ remontnik/cli.py
@@ -1,14 +1,22 @@
+import argparse
 from pprint import pprint
 
 from .opensky import find_close
 
 
+MAX_DIST = 450000
+PARIS_LAT, PARIS_LON = (48.85341, 2.3488)
+
+
 def cli():
-    MAX_DIST = 450000
-    PARIS_LAT, PARIS_LON = (48.85341, 2.3488)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('lat', type=float, default=PARIS_LAT)
+    parser.add_argument('lon', type=float, default=PARIS_LON)
+    parser.add_argument('dist', type=int, default=MAX_DIST)
+    args = parser.parse_args()
 
     pprint(
         find_close(
-            target_lat=PARIS_LAT,
-            target_lon=PARIS_LON,
-            max_dist=MAX_DIST))
+            target_lat=args.lat,
+            target_lon=args.lon,
+            max_dist=args.dist))
