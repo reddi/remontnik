8c1d98e0c3ff6c8a7b8af521e458dcd03bcb1bab
Eugene Chekalov <evgchekalov@gmail.com>
2018-02-08 18:55:30 +0300

    fix using argparse

--- remontnik/cli.py
+++ remontnik/cli.py
@@ -10,9 +10,9 @@ PARIS_LAT, PARIS_LON = (48.85341, 2.3488)
 
 def cli():
     parser = argparse.ArgumentParser()
-    parser.add_argument('lat', type=float, default=PARIS_LAT)
-    parser.add_argument('lon', type=float, default=PARIS_LON)
-    parser.add_argument('dist', type=int, default=MAX_DIST)
+    parser.add_argument('--lat', type=float, default=PARIS_LAT)
+    parser.add_argument('--lon', type=float, default=PARIS_LON)
+    parser.add_argument('--dist', type=int, default=MAX_DIST)
     args = parser.parse_args()
 
     pprint(
